b5acec127bd529e368efc8b7a65e00fa3b065e5b
PeterHuang <qsmy122011@gmail.com>
2021-11-10 17:20:43 +0800

    2.1 Method face.VideoRecognition

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ import sys
 import os
 
 
+def compare(face1, face2):
+    ...
+
+
 class FaceRecognizer:
         def __init__(self, face_det_weight, feature_extract_weight, save_crop_img_dir, feature_dict, use_milvus):
             super(FaceRecognizer, self).__init__()
@@ -29,6 +33,22 @@ class FaceRecognizer:
             self.feature_dict_path = feature_dict
             self.feat_dict    = json.load(open(self.feature_dict_path, "r"))
 
+        #region ---------------------------------------  some utils -------------------------------------
+        def img_to_milvus_feature(self):
+            ...
+
+        def img_to_json_feature(self):
+            ...
+
+        def crop_img_from_video(self, source):
+            ...
+
+        def save_img_from_video(self, crop_img_list, name):
+            ...
+
+        #endregion ------------------------------------- some utils -------------------------------------
+
+        #region -------------------------------------- 3 Major Functionalities ------------------------------------------
 
         def Register(self, source, user_name):
             # get the input image from the croped image list
@@ -50,24 +70,6 @@ class FaceRecognizer:
                 # save then as json file
                 json.dump(self.feat_dict, open(self.feature_dict_path, "w")) # dump the read_json into a new place(aka. self.feature_dict_path)
 
-        #region ---------------------------------------  some utils -------------------------------------
-        def img_to_milvus_feature(self):
-            ...
-
-        def img_to_json_feature(self):
-            ...
-
-        def crop_img_from_video(self, source):
-            ...
-
-        def save_img_from_video(self, crop_img_list, name):
-            ...
-
-        #endregion ------------------------------------- some utils -------------------------------------
-
-
-        #region -------------------------------------- 3 Major Functionalities ------------------------------------------
-
         def ImageRecognize(self, source):
             image = cv2.imread(source)
             bboxes = self.detector(image) # 1 images ---> n faces
@@ -75,7 +77,7 @@ class FaceRecognizer:
             if bboxes:
                 for box in bboxes:
                     x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
-                    M = self.get_align_matrix(box)
+                    M = self.get_alignment_matrix(box)
                     aligned_croped_img = self.crop_and_align(M, image.copy())
                     aligned_croped_img = Image.fromarray(aligned_croped_img)
                     feature_test = self.extractor(aligned_croped_img)
@@ -121,24 +123,69 @@ class FaceRecognizer:
                         cv2.destroyAllWindows()
                     #endregion --------------------------------Thresholding and Visulizing ---------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         def VideoRecognize(self, source):
-            ...
+            # recognize face in the video or videostream
+            cap = cv2.VideoCapture(source, cv2.CAP_DSHOW) # short for directshow via video input
 
-        def align(self, M, image):
+            while True: # keep reading the video stream
+                ret, frame = cap.read()
+
+                if not ret:
+                    continue
+
+                t0 = time.time()
+                bboxes = self.detector(frame)
+                t1 = time.time()
+                print(f"detection: ----> {t1 - t0}s")
+
+                if bboxes:
+                    for box in bboxes:
+                        x1, y1, x2, y2 = box[0], box[1],box[2], box[3]
+                        M = self.get_alignment_matrix(box)
+                        aligned_croped_img = self.align(M, frame.copy())
+                        aligned_croped_img = Image.fromarray(aligned_croped_img)
+                        feature_test = self.extractor(aligned_croped_img)
+
+                        # region ----------------------- two methods to get the max_score class------------------------
+                        if self.use_milvus:
+                            ...
+                        else:
+                            cls_list = []
+                            max_cls_scores = []
+
+                            for cls in self.feat_dict:
+                                cls_simi_scores = []
+                                cls_list.append(cls)
+
+                                for feat in self.feat_dict[cls]:
+                                    simi_score = compare(torch.tensor(feature_test), torch.tensor(feat))
+                                    cls_simi_scores.append(simi_score)
+
+                                max_cls_scores.append(max(cls_simi_scores))
+                        # endregion ----------------------- two methods to get the max_score class ------------------------
+
+                        # region --------------------------------Thresholding and Visulizing ---------------------------------------------
+                        if max_cls_scores > 0.4:
+                            print(f"similarity:", max(max_cls_scores))
+                            index = np.argmax(max_cls_scores)
+                            pred_cls = cls_list[index]
+
+                            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                            cv2.putText(frame, pred_cls, (x1, y1 - 2), 0, 3, [255, 255, 255], thickness=3,
+                                        lineType=cv2.LINE_AA)
+
+
+
+                        # endregion --------------------------------Thresholding and Visulizing ---------------------------------------------
+
+                    cv2.imshow("1", frame)
+                    t_end = time.time()
+                    print(f"one frame ---> {t_end - t0}s \n")
+
+                if cv2.waitKey(10) & 0xFF == ord("q"):
+                    break
+
+        def get_alignment_matrix(self, M, image):
             ...
         # ------------------------------------------- 3 Major Functionalities ------------------------------------------
         #endregion
